af40aafdfdfc4c8374d886f90baee8ad19752a3d
Jennie Kadariya <kadariyajennie3579@gmail.com>
2022-09-07 10:01:25 +0545

    project started

---
+++ intro.py
@@ -0,0 +1,22 @@
+# This is mike.
+"""
+print("i am from batch33")
+
+print("52=52") 
+
+#Button calling-->Dail
+def dail (phone-num):
+    
+    print(f"{phone_num}/n Calling...")
+
+   """ 
+
+
+name = input("Please provide your name")
+age=input ("Please provide your age ")
+print(f"Hi!,your name is {name} and you are{age} years old")
+
+
+profession =input("Please provide your profession") 
+income=input ("Please provide your income")
+print(f"Hi!,your profession is {profession} and your income is {income}")
